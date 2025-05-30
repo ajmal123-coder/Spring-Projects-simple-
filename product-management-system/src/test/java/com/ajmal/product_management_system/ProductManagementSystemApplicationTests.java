5b14578295146c061a20a4527f08f533b2f6b556
ajmal123-coder <ajmalsadhath@gmail.com>
2025-05-30 13:41:20 +0530

    spring structure added

---
+++ product-management-system/src/test/java/com/ajmal/product_management_system/ProductManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ajmal.product_management_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
